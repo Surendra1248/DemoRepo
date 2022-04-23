f7d1402bc8eb73cc06253c8aa30958d9a5bbabeb
Surendra1248 <102716227+Surendra1248@users.noreply.github.com>
2022-04-23 09:58:56 +0530

    Update TwoTest.java

--- FrameWorkProj/src/test/java/tests/TwoTest.java
+++ FrameWorkProj/src/test/java/tests/TwoTest.java
@@ -11,6 +11,7 @@ public class TwoTest extends Base{
 	public WebDriver driver;
 	@Test
 	public void testTwo() throws IOException, InterruptedException {
+		Sysytem.out.println("Changes made by sruthi");
 	driver=initializeDriver();
 	driver.get("https://www.google.co.in/");
 	Thread.sleep(3000);
