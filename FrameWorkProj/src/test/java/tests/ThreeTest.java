b0af1c4aaa4e2a4e2fb3a51634af043a272f5d5c
Vishnu <vishnu@test.com>
2022-04-23 09:53:09 +0530

    This is commited by vishnu

--- FrameWorkProj/src/test/java/tests/ThreeTest.java
+++ FrameWorkProj/src/test/java/tests/ThreeTest.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
 import resources.Base;
 
 public class ThreeTest extends Base {
 	public WebDriver driver;
 	@Test
 	public void testThree() throws IOException, InterruptedException {
+		System.out.println("Vishnu has updated this code with this statment");
 		driver=initializeDriver();
 		driver.get("https://www.selenium.dev/");
 		Thread.sleep(3000);
